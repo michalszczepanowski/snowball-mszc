a74078c8ad804f4a60fbcc9d0825e79fd33d0793
Michał Szczepanowski <michal.szczepanowski@allegro.pl>
2021-12-13 13:09:08 +0100

    add bigquery export

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,10 +1,19 @@
 package hello;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.ServiceOptions;
+import com.google.cloud.bigquery.storage.v1.*;
+import com.google.protobuf.Descriptors;
 
+
+import java.io.IOException;
+import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -59,6 +68,9 @@ public class Application {
 
   @PostMapping("/**")
   public String index(@RequestBody ArenaUpdate arenaUpdate) {
+    writeCommittedStream.send(arenaUpdate.arena);
+
+
     PlayerState self = arenaUpdate.arena.state.get(arenaUpdate._links.self.href);
 
     if (self.wasHit) {
@@ -138,6 +150,61 @@ public class Application {
             .filter(entry -> self.x - entry.getValue().x > 0)
             .anyMatch(entry -> entry.getValue().y == self.y);
   }
+
+
+    static class WriteCommittedStream {
+
+        final JsonStreamWriter jsonStreamWriter;
+
+        public WriteCommittedStream(String projectId, String datasetName, String tableName) throws IOException, Descriptors.DescriptorValidationException, InterruptedException {
+
+            try (BigQueryWriteClient client = BigQueryWriteClient.create()) {
+
+                WriteStream stream = WriteStream.newBuilder().setType(WriteStream.Type.COMMITTED).build();
+                TableName parentTable = TableName.of(projectId, datasetName, tableName);
+                CreateWriteStreamRequest createWriteStreamRequest =
+                        CreateWriteStreamRequest.newBuilder()
+                                .setParent(parentTable.toString())
+                                .setWriteStream(stream)
+                                .build();
+
+                WriteStream writeStream = client.createWriteStream(createWriteStreamRequest);
+
+                jsonStreamWriter = JsonStreamWriter.newBuilder(writeStream.getName(), writeStream.getTableSchema()).build();
+            }
+        }
+
+        public ApiFuture<AppendRowsResponse> send(Arena arena) {
+            Instant now = Instant.now();
+            JSONArray jsonArray = new JSONArray();
+
+            arena.state.forEach((url, playerState) -> {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("x", playerState.x);
+                jsonObject.put("y", playerState.y);
+                jsonObject.put("direction", playerState.direction);
+                jsonObject.put("wasHit", playerState.wasHit);
+                jsonObject.put("score", playerState.score);
+                jsonObject.put("player", url);
+                jsonObject.put("timestamp", now.getEpochSecond() * 1000 * 1000);
+                jsonArray.put(jsonObject);
+            });
+
+            return jsonStreamWriter.append(jsonArray);
+        }
+
+    }
+
+    final String projectId = ServiceOptions.getDefaultProjectId();
+    final String datasetName = "snowball";
+    final String tableName = "events";
+
+    final WriteCommittedStream writeCommittedStream;
+
+    public Application() throws Descriptors.DescriptorValidationException, IOException, InterruptedException {
+        writeCommittedStream = new WriteCommittedStream(projectId, datasetName, tableName);
+    }
+
 
 }
 
